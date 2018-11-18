b88760af11c3bd984000d5b07460cbc5e9d5290d
CAROLINA RIBEIRO <42756880+carolribs@users.noreply.github.com>
2018-11-18 12:12:55 -0200

    Pesquisar produtos

---
+++ PesquisaPHP.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        
+        <title> Pesquisar produtos </title>
+        
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+    <script type="text/javascript" src="javascriptpersonalizado.js"></script>
+        
+   
+        
+        
+    </head>
+    
+    <body>
+        
+        <h1> Pesquisar produtos </h1>
+        <form method ="POST " action="busca.php" id = "forn-pesquisa" >
+        
+         <fieldset><legend> Pesquisar </legend>
+             <input type="text" name="pesquisa" id = "pesquisa" placeholder="Digite aqui"> 
+         <input type="submit" name = "pesquisar" value = "Pesquisar" >  
+         </fieldset>
+        </form>
+     
+    </body>
+
+</html>
